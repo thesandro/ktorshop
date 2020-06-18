59b27f3893f033c5efed939682d59e03e5b5184a
Sandro <thesandro1998@gmail.com>
2020-06-19 02:54:37 +0400

    tiny fix

--- src/Application.kt
+++ src/Application.kt
@@ -70,7 +70,7 @@ object ApiClient {
     val getApiClient: ApiInterface by lazy {
         val retrofit = Retrofit.Builder()
                 .baseUrl(BASE_URL)
-                .addConverterFactory(ScalarsConverterFactory.create())
+                .addConverterFactory(JacksonConverterFactory.create())
                 .build()
 
         retrofit.create(ApiInterface::class.java)
